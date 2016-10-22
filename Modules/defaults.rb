4b533186ebda065f550e1c209a1abb476add4feb
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-10-22 21:34:55 +0900

    itamae, serverspec

--- Modules/defaults.rb
+++ Modules/defaults.rb
@@ -6,6 +6,8 @@ def defaults_load(source_file)
   defaults_file = File.expand_path(File.dirname(source_file)) \
                   + "/../defaults/main.yml"
   if File.exists?(defaults_file)
-    return YAML.load_file(defaults_file)
+    return(YAML.load_file(defaults_file))
+  else
+    return({})
   end
 end
