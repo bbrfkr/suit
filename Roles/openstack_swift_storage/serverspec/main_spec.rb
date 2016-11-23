a21d5b193c25da545b89536f814e140df10d7548
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-11-23 18:11:57 +0900

    itamae, serverspec, kicker

--- Roles/openstack_swift_storage/serverspec/main_spec.rb
+++ Roles/openstack_swift_storage/serverspec/main_spec.rb
@@ -48,12 +48,42 @@ describe ("openstack_swift_storage") do
   end
 
   describe ("check owner and permission to recon directory") do
-    describe command("(ls -ld /var/cache/swift && ls -lR /var/cache/swift) | grep -e \"[d|-]\\([r|-][w|-][x|-]\\)\\{3\\}\" | grep -v \"root swift\"") do
-      its(:exit_status) { should_not eq 0 }
+    describe file("/var/cache/swift") do
+      it { should be_directory }
+      it { should be_mode 775 }
+      it { should be_owned_by "root" }
+      it { should be_grouped_into "swift" }
     end
+  end
 
-    describe command("(ls -ld /var/cache/swift && ls -lR /var/cache/swift) | grep -e \"[d|-]\\([r|-][w|-][x|-]\\)\\{3\\}\" | grep -v -e \"[d|-]rwxrwxr-x\"") do
+  describe ("check rings file exist") do
+    rings = ["account.ring.gz", "container.ring.gz", "object.ring.gz"]
+    rings.each do |ring|
+      describe file("/etc/swift/" + ring) do
+        it { should exist }
+      end
+    end
+  end
+
+  describe ("check ownership of the config directory") do
+    describe command("(ls -ld /etc/swift && ls -lR /etc/swift) | grep -e \"[d|-]\\([r|-][w|-][x|-]\\)\\{3\\}\" | grep -v \"root swift\"") do
       its(:exit_status) { should_not eq 0 }
     end
   end
+
+  describe ("check swift services are enabled and running") do
+    services = ["openstack-swift-account", "openstack-swift-account-auditor", \
+                "openstack-swift-account-reaper", "openstack-swift-account-replicator", \
+                "openstack-swift-container", "openstack-swift-container-auditor", \
+                "openstack-swift-container-replicator", "openstack-swift-container-updater", \
+                "openstack-swift-object", "openstack-swift-object-auditor", \
+                "openstack-swift-object-replicator", "openstack-swift-object-updater"]
+    services.each do |srv|
+      describe service("srv") do
+        it { should be_enabled }
+        it { should be_running }
+      end
+    end
+  end
 end
+
