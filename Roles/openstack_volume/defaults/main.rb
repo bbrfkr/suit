0cb8cbdbc0e96bc4da87f32a6fe46b03b6d1e6ca
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-11-26 12:20:12 +0000

    itamae, serverspec

---
+++ Roles/openstack_volume/defaults/main.rb
@@ -0,0 +1,15 @@
+---
+openstack_volume:
+  credential:
+    project_domain: default
+    user_domain: default
+    project: demo
+    user: demo
+    password: password
+    auth_url: http://controller:5000/v3
+    identity_api_version:  3
+    image_api_version: 2
+  volumes:
+    - image: myvolume
+      size: 1
+
