bbeb3ded815d7106a67871dd47ff5064a3cfc585
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-11-27 13:54:42 +0000

    bug fix and adapt non root user

--- Roles/openstack_network/itamae/main.rb
+++ Roles/openstack_network/itamae/main.rb
@@ -60,7 +60,7 @@ nics.each do |nic|
     action :edit
     notifies :restart, "service[network]"
     block do |content|
-      content.gsub!(/^PEERDNS=yes/, "PEERDNS=no")
+      content.gsub!(/^PEERDNS="?yes"?/, "PEERDNS=no")
     end
   end
 end
@@ -73,20 +73,27 @@ local_ruby_block "reboot server" do
   block do
     begin
       if reboot_flag
-        if ENV['CONN_IDKEY'] == nil
-          Net::SSH.start(ENV['CONN_HOST'],
-                         ENV['CONN_USER'],
-                         :password => ENV['CONN_PASS'],
-                         :port => ENV['CONN_PORT']) do |ssh|
+        opt = { :port => ENV['CONN_PORT'] }
+        if ENV['CONN_IDKEY'] != nil
+          opt[:keys] = ["Env/" + ENV['CONN_IDKEY']]
+        end
+        if ENV['CONN_PASSPHRASE'] != nil
+          opt[:passphrase] = ENV['CONN_PASSPHRASE']
+        end
+        if ENV['CONN_PASS'] != nil
+          opt[:password] = ENV['CONN_PASS']
+        end
+        Net::SSH.start(ENV['CONN_HOST'], ENV['CONN_USER'], opt) do |ssh|
+          if ENV['CONN_USER'] == "root"
             ssh.exec "reboot"
-          end
-        else
-          Net::SSH.start(ENV['CONN_HOST'],
-                         ENV['CONN_USER'],
-                         :keys => ENV['CONN_IDKEY'],
-                         :passphrase => ENV['CONN_PASS'],
-                         :port => ENV['CONN_PORT']) do |ssh|
-            ssh.exec "reboot"
+          else
+            channel = ssh.open_channel do |ch|
+              channel.request_pty do |ch, success|
+                raise "could not obtain pty" if !success
+              end
+              channel.exec "sudo reboot"
+            end
+            ssh.loop
           end
         end
       end
