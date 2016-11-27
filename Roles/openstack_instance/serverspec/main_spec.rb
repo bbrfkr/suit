a08a3abc6e62f07d9cdd8a616c5c03b0a0673d5c
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-11-27 04:48:42 +0000

    adapt delete instance

--- Roles/openstack_instance/serverspec/main_spec.rb
+++ Roles/openstack_instance/serverspec/main_spec.rb
@@ -8,90 +8,109 @@ credential_str = openstack_credential(property['openstack_instance']['credential
 
 describe ("openstack_instances") do
   instances.each do |instance|
+    # check prameter "state"
+    if instance['state'] != "present" && instance['state'] != "absent"
+      fail 'parameter "state" should be "present" or "absent" '
+    end
+
     describe ("instance \"#{ instance['name'] }\"") do
-      describe ("check instance is active") do
-        cmd = <<-"EOS"
-          #{ credential_str } \\
-          openstack server show #{ instance['name'] } | \\
-          grep status | \\
-          awk '{ print $4 }'
-        EOS
-        describe command(cmd) do
-          its(:stdout) { should match /^ACTIVE$/ }
+      if instance['state'] == "present"
+        describe ("check instance is active") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] } | \\
+            grep status | \\
+            awk '{ print $4 }'
+          EOS
+          describe command(cmd) do
+            its(:stdout) { should match /^ACTIVE$/ }
+          end
         end
-      end
-
-      describe ("check instance name is appropriate") do
-        cmd = <<-"EOS"
-          #{ credential_str } \\
-          openstack server show #{ instance['name'] } | \\
-          grep name | \\
-          awk '{ print $4 }'
-        EOS
-        describe command(cmd) do
-          its(:stdout) { should match /^#{ instance['name'] }$/ }
+  
+        describe ("check instance name is appropriate") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] } | \\
+            grep name | \\
+            awk '{ print $4 }'
+          EOS
+          describe command(cmd) do
+            its(:stdout) { should match /^#{ instance['name'] }$/ }
+          end
         end
-      end
-
-      describe ("check image used by instance is appropriate") do
-        cmd = <<-"EOS"
-          #{ credential_str } \\
-          openstack server show #{ instance['name'] } | \\
-          grep image | \\
-          awk '{ print $4 }'
-        EOS
-        describe command(cmd) do
-          its(:stdout) { should match /^#{ instance['image'] }$/ }
+  
+        describe ("check image used by instance is appropriate") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] } | \\
+            grep image | \\
+            awk '{ print $4 }'
+          EOS
+          describe command(cmd) do
+            its(:stdout) { should match /^#{ instance['image'] }$/ }
+          end
         end
-      end
-
-      describe ("check falvor used by instance is appropriate") do
-        cmd = <<-"EOS"
-          #{ credential_str } \\
-          openstack server show #{ instance['name'] } | \\
-          grep flavor | \\
-          awk '{ print $4 }'
-        EOS
-        describe command(cmd) do
-          its(:stdout) { should match /^#{ instance['flavor'] }$/ }
+  
+        describe ("check falvor used by instance is appropriate") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] } | \\
+            grep flavor | \\
+            awk '{ print $4 }'
+          EOS
+          describe command(cmd) do
+            its(:stdout) { should match /^#{ instance['flavor'] }$/ }
+          end
         end
-      end
-
-      describe ("check security groups set to instance is appropriate") do
-        cmd = <<-"EOS"
-          #{ credential_str } \\
-          openstack server show #{ instance['name'] } | \\
-          grep security_groups | \\
-          awk -F\\| '{ print $3 }'
-        EOS
-        describe command(cmd) do
-          instance['security_groups'].each do |sec_grp|
-            its(:stdout) { should match /u'#{ sec_grp }'/ }
+  
+        describe ("check security groups set to instance is appropriate") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] } | \\
+            grep security_groups | \\
+            awk -F\\| '{ print $3 }'
+          EOS
+          describe command(cmd) do
+            instance['security_groups'].each do |sec_grp|
+              its(:stdout) { should match /u'#{ sec_grp }'/ }
+            end
+          end
+        end
+  
+        describe ("check key pair set to instance is appropriate") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] } | \\
+            grep key_name | \\
+            awk '{ print $4 }'
+          EOS
+          describe command(cmd) do
+            its(:stdout) { should match /^#{ instance['key_pair'] }$/ }
+          end
+        end
+  
+        describe ("check network used by instance is appropriate") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] } | \\
+            grep addresses | \\
+            awk '{ print $4 }'
+          EOS
+          describe command(cmd) do
+            its(:stdout) { should match /^#{ instance['network'] }=/ }
           end
         end
       end
 
-      describe ("check key pair set to instance is appropriate") do
-        cmd = <<-"EOS"
-          #{ credential_str } \\
-          openstack server show #{ instance['name'] } | \\
-          grep key_name | \\
-          awk '{ print $4 }'
-        EOS
-        describe command(cmd) do
-          its(:stdout) { should match /^#{ instance['key_pair'] }$/ }
-        end
-      end
-
-      describe ("check network used by instance is appropriate") do
-        cmd = <<-"EOS"
-          #{ credential_str } \\
-          openstack server show #{ instance['name'] } | \\
-          grep addresses | \\
-          awk '{ print $4 }'
-        EOS
-        describe command(cmd) do
-          its(:stdout) { should match /^#{ instance['network'] }=/ }
+      if instance['state'] == "absent"
+        describe ("check instance doesn't exist") do
+          cmd = <<-"EOS"
+            #{ credential_str } \\
+            openstack server show #{ instance['name'] }
+          EOS
+          describe command(cmd) do
+            its(:exit_status) { should_not eq 0 }
+          end
         end
       end
     end
